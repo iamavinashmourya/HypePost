c97df931960e05d1085cf97720be22419adc3687
iamavinashmourya <avinashgoweb@gmail.com>
2024-06-25 22:32:12 +0530

    Editor Page Component

---
+++ frontend/src/components/blog-editor.component.jsx
@@ -0,0 +1,7 @@
+const BlogEditor = () => {
+    return (
+        <h1>Blog Editor From Component JSX File</h1>
+    )
+}
+
+export default BlogEditor;
---
+++ frontend/src/components/publish-form.component.jsx
@@ -0,0 +1,7 @@
+const PublishForm = () => {
+    return (
+        <h1>Publish From Component JSX File</h1>
+    )
+}
+
+export default PublishForm;
