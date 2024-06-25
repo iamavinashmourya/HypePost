0013d71dde6ff6d687aff472cf5d939e68fd258f
iamavinashmourya <avinashgoweb@gmail.com>
2024-06-25 22:48:26 +0530

    Blog Editor Navbar

--- frontend/src/components/blog-editor.component.jsx
+++ frontend/src/components/blog-editor.component.jsx
@@ -1,6 +1,29 @@
+import { Link } from "react-router-dom";
+import logo from "../imgs/logo.png"
+
 const BlogEditor = () => {
     return (
-        <h1>Blog Editor From Component JSX File</h1>
+        <>
+        
+        <nav className="navbar">
+            <Link to="/" className="flex-none w-10">
+                <img src={logo}/>
+            </Link>
+            <p className="max-md:hidden text-black line-clamp-1 w-full">
+                New Blog
+            </p>
+
+            <div className="flex gap-4 ml-auto">
+                <button className="btn-dark py-2">
+                    Publish
+                </button>
+                <button className="btn-light py-2">
+                    Save Draft
+                </button>
+            </div>
+        </nav>
+
+        </>
     )
 }
 
