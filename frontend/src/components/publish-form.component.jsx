d9808044b3b861c1707470b7aee732a1557d42ad
iamavinashmourya <avinashgoweb@gmail.com>
2024-07-13 10:56:49 +0530

    Publishing the blog

--- frontend/src/components/publish-form.component.jsx
+++ frontend/src/components/publish-form.component.jsx
@@ -3,13 +3,20 @@ import AnimationWrapper from "../common/page-animation";
 import { useContext } from "react";
 import { EditorContext } from "../pages/editor.pages";
 import Tag from "./tags.component";
+import axios from "axios";
+import { UserContext } from "../App";
+import { useNavigate } from "react-router-dom";
 
 const PublishForm = () => {
 
     let characterLimit = 200;
     let tagLimit = 10;
 
-    let { blog, blog: { banner, title, tags, des }, setEditorState, setBlog } = useContext(EditorContext)
+    let { blog, blog: { banner, title, tags, des, content }, setEditorState, setBlog } = useContext(EditorContext)
+
+    let { userAuth: { access_token } } = useContext(UserContext);
+
+    let navigate = useNavigate();
 
     const handleCloseEvent = () => {
         setEditorState("editor")
@@ -50,6 +57,58 @@ const PublishForm = () => {
 
             e.target.value = "";
         }
+    }
+
+    const publishBlog = (e) => {
+
+        if (e.target.className.includes("disable")){
+            return;
+        }
+            
+
+        if (!title.length) {
+            return toast.error("Write blog title before publishing")
+        }
+
+        if (!des.length || des.length > characterLimit) {
+            return toast.error(`Write description about your blog within ${characterLimit} characters to publish`)
+        }
+
+        if (!tags.length) {
+            return toast.error("Enter at least 1 tag to help us rank your blog")
+        }
+
+        let loadingToast = toast.loading("Publishing....");
+
+        e.target.classList.add('disable');
+
+        let blogObj = {
+            title, banner, des, content, tags, draft: false
+        }
+
+        axios.post(import.meta.env.VITE_SERVER_DOMAIN + "/create-blog", blogObj, {
+            headers: {
+                'Authorization': `Bearer ${access_token}`
+            }
+        })
+        .then(() => {
+
+            e.target.classList.remove('disable');
+
+            toast.dismiss(loadingToast);
+            toast.success("Published 👍");
+
+            setTimeout(() => {
+                navigate("/")
+            }, 500);
+
+        })
+        .catch(( { response } ) => {
+            e.target.classList.remove('disable');
+            toast.dismiss(loadingToast);
+
+            return toast.error(response.data.error)
+        })
     }
 
     return (
@@ -112,7 +171,9 @@ const PublishForm = () => {
 
                     <p className="mt-1 mb-4 text-dark-grey text-right">{ tagLimit - tags.length } Tags Left</p>
 
-                    <button className="btn-dark px-8">Publish</button>
+                    <button className="btn-dark px-8"
+                        onClick={publishBlog}
+                    >Publish</button>
                     
                 </div>
 
