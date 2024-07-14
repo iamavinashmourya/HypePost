b9a67325b02f82b5d3acb0af89bb5cb9f7e1d77a
iamavinashmourya <avinashgoweb@gmail.com>
2024-07-14 14:27:15 +0530

    Home Page - Blog Card

--- frontend/src/pages/home.page.jsx
+++ frontend/src/pages/home.page.jsx
@@ -3,6 +3,7 @@ import AnimationWrapper from "../common/page-animation";
 import InPageNavigation from "../components/inpage-navigation.component";
 import { useEffect, useState } from "react";
 import Loader from "../components/loader.component"
+import BlogPostCard from "../components/blog-post.component";
 
 const HomePage = () => {
 
@@ -35,7 +36,9 @@ const HomePage = () => {
                             {
                                 blogs == null ? <Loader />: 
                                 blogs.map((blog, i) => {
-                                    return <h1 key={i}>{ blog.title }</h1>
+                                    return <AnimationWrapper transition={{ duration: 1, delay: i*.1 }} key={i}>
+                                        <BlogPostCard content={blog} author={blog.author.personal_info} />
+                                    </AnimationWrapper>
                                 })
                                 
                             }
