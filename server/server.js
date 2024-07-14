4388977ed463005b12308ef7df70702834a12115
iamavinashmourya <avinashgoweb@gmail.com>
2024-07-14 14:43:01 +0530

    Home Page - trending blogs route

--- server/server.js
+++ server/server.js
@@ -240,6 +240,22 @@ server.post("/google-auth", async (req, res) => {
 
 })
 
+server.get("/trending-blogs", (req, res) => {
+
+    Blog.find({ draft: false })
+    .populate("author", "personal_info.profile_img personal_info.username personal_info.fullname -_id")
+    .sort({ "activity.total_read": -1, "activity.total_likes": -1, "publishedAt": -1 })
+    .select("blog_id title publishedAt -_id")
+    .limit(5)
+    .then(blogs => {
+        return res.status(200).json({ blogs })
+    })
+    .catch(err => {
+        return res.status(500).json({ error: err.message })
+    })
+
+})
+
 server.get('/latest-blogs', (req, res) => {
 
     let maxLimit = 5;
