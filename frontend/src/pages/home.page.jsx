f8fbcb24f27c98eca334d7b6a3061f7d7c130306
iamavinashmourya <avinashgoweb@gmail.com>
2024-07-20 19:34:29 +0530

    NoDataMessage Component

--- frontend/src/pages/home.page.jsx
+++ frontend/src/pages/home.page.jsx
@@ -6,6 +6,7 @@ import Loader from "../components/loader.component"
 import BlogPostCard from "../components/blog-post.component";
 import MinimalBlogPost from "../components/nobanner-blog-post.component";
 import { activeTabRef } from "../components/inpage-navigation.component";
+import NoDataMessage from "../components/nodata.component";
 
 const HomePage = () => {
 
@@ -98,26 +99,30 @@ const HomePage = () => {
 
                         <>
                             {
-                                blogs == null ? <Loader /> :
-                                    blogs.map((blog, i) => {
-                                        return <AnimationWrapper transition={{ duration: 1, delay: i * .1 }} key={i}>
-                                            <BlogPostCard content={blog} author={blog.author.personal_info} />
-                                        </AnimationWrapper>
-                                    })
-
-                            }
+                                blogs == null ? (  <Loader />  ) : (
+                                    blogs.length ? 
+                                        blogs.map((blog, i) => {
+                                            return ( <AnimationWrapper transition={{ duration: 1, delay: i * .1 }} key={i}>
+                                                <BlogPostCard content={blog} author={blog.author.personal_info} />
+                                            </AnimationWrapper>
+                                            );
+                                        })
+                                    : <NoDataMessage message="No Blogs Published" />
+                            )}
 
                         </>
 
                         {
-                            trendingBlogs == null ? <Loader /> :
-                                trendingBlogs.map((blog, i) => {
-                                    return <AnimationWrapper transition={{ duration: 1, delay: i * .1 }} key={i}>
-                                        <MinimalBlogPost blog={blog} index={i} />
-                                    </AnimationWrapper>
-                                })
-
-                        }
+                            trendingBlogs == null ? ( <Loader /> ) : ( 
+                                trendingBlogs.length ? 
+                                    trendingBlogs.map((blog, i) => {
+                                        return ( <AnimationWrapper transition={{ duration: 1, delay: i * .1 }} key={i}>
+                                            <MinimalBlogPost blog={blog} index={i} />
+                                        </AnimationWrapper>
+                                        );
+                                    })
+                                : <NoDataMessage message="No trending blogs found" />
+                        )}
 
 
 
@@ -150,14 +155,17 @@ const HomePage = () => {
                         <h1 className="font-medium text-xl mb-8">Trending <i className="fi fi-rr-arrow-trend-up"></i></h1>
 
                         {
-                            trendingBlogs == null ? <Loader /> :
-                                trendingBlogs.map((blog, i) => {
-                                    return <AnimationWrapper transition={{ duration: 1, delay: i * .1 }} key={i}>
-                                        <MinimalBlogPost blog={blog} index={i} />
-                                    </AnimationWrapper>
-                                })
+                            trendingBlogs == null ? ( <Loader /> ) : (
+                                trendingBlogs.length ? 
+                                    trendingBlogs.map((blog, i) => {
+                                        return ( <AnimationWrapper transition={{ duration: 1, delay: i * .1 }} key={i}>
+                                            <MinimalBlogPost blog={blog} index={i} />
+                                        </AnimationWrapper>
+                                        );
+                                    })
+                                : <NoDataMessage message="No trending blogs found" />
 
-                        }
+                        )}
 
                     </div>
 
