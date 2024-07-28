8c1349b65e55f57027d8626e9ff5988a2896856c
iamavinashmourya <avinashgoweb@gmail.com>
2024-07-28 19:34:49 +0530

    Search Route

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -5,6 +5,7 @@ import { createContext, useEffect, useState } from "react";
 import { lookInSession } from "./common/session";
 import Editor from "./pages/editor.pages";
 import HomePage from "./pages/home.page";
+import SearchPage from "./pages/search.page";
 
 export const UserContext = createContext({})
 
@@ -28,6 +29,7 @@ const App = () => {
                 <Route index element={<HomePage />}/>
                 <Route path="/signin" element={<UserAuthForm type="sign-in"/>} /> 
                 <Route path="/signup" element={<UserAuthForm type="sign-up"/>} />
+                <Route path="search/:query" element={<SearchPage />} />
             </Route>
             
         </Routes>
---
+++ frontend/src/pages/search.page.jsx
@@ -0,0 +1,7 @@
+const SearchPage = () => {
+    return (
+        <h1>Search Page jsx component</h1>
+    )
+}
+
+export default SearchPage;
