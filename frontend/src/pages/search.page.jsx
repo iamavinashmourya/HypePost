9d8830b886d27db7b6751d338808abb0470f8e3a
iamavinashmourya <avinashgoweb@gmail.com>
2024-08-29 20:43:21 +0530

    Search User Card

--- frontend/src/pages/search.page.jsx
+++ frontend/src/pages/search.page.jsx
@@ -8,6 +8,7 @@ import NoDataMessage from "../components/nodata.component";
 import LoadMoreDataBtn from "../components/load-more.component";
 import axios from "axios";
 import { filterPaginationData } from "../common/filter-pagination-data";
+import UserCard from "../components/usercard.component";
 
 const SearchPage = () => {
 
@@ -40,7 +41,7 @@ const SearchPage = () => {
     }
 
     const  fetchUsers = () => {
-        axios.post(import.meta.env.VITE_SERVER_DOMAIN + "/search-blogs", { query })
+        axios.post(import.meta.env.VITE_SERVER_DOMAIN + "/search-users", { query })
         .then(({ data: { users } }) => {
             setUsers(users);
         })
@@ -57,6 +58,23 @@ const SearchPage = () => {
     const resetState = () => {
         setBlog(null);
         setUsers(null);
+    }
+
+    const UserCardWrapper = () => {
+        return (
+            <>
+                {
+                    users == null ? <Loader />:
+                        users.length ?
+                            users.map((user, i) => {
+                                return <AnimationWrapper key={i} transition={{ duration: 1, delay: i*0.08 }}>
+                                    <UserCard user={user} />
+                                </AnimationWrapper>
+                            })
+                        : <NoDataMessage message= "No user found" />
+                }
+            </>
+        )
     }
 
     return (
@@ -80,7 +98,17 @@ const SearchPage = () => {
                         <LoadMoreDataBtn state={blogs} fetchDataFun={searchBlogs} />
                     </>
 
+                    <UserCardWrapper />
+
                 </InPageNavigation>
+            </div>
+
+            <div className="min-w-[40%] lg:min-w-[350px] max-w-min border-l border-grey pl-8 pt-3 max-md:hidden">
+
+                <h1 className="font-medium text-xl mb-8">User related to search <i className="fi fi-rr-user mt-1"></i></h1>
+
+                <UserCardWrapper />
+
             </div>
 
         </section>
