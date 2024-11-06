199b7a362e73fcbfae49173e1dc417de657982f2
iamavinashmourya <avinashgoweb@gmail.com>
2024-11-06 20:43:19 +0530

    User profile page - About User

--- frontend/src/pages/profile.page.jsx
+++ frontend/src/pages/profile.page.jsx
@@ -4,6 +4,7 @@ import { Link, useParams } from "react-router-dom";
 import AnimationWrapper from "../common/page-animation";
 import Loader from "../components/loader.component";
 import { UserContext } from "../App";
+import AboutUser from "../components/about.component";
 
 export const profileDataStrcuture = {
     personal_info: {
@@ -80,6 +81,8 @@ const ProfilePage = () => {
                             }
                             
                         </div>
+
+                        <AboutUser className="max-md:hidden" bio={bio} social_links={social_links} joinedAt={joinedAt} />
 
                     </div>
 
