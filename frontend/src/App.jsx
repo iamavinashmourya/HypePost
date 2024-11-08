017d938e8b31826b9b4027303c8660ef55638def
iamavinashmourya <avinashgoweb@gmail.com>
2024-11-08 16:57:13 +0530

    Blog Page Route

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -8,6 +8,7 @@ import HomePage from "./pages/home.page";
 import SearchPage from "./pages/search.page";
 import PageNotFound from "./pages/404.page";
 import ProfilePage from "./pages/profile.page";
+import BlogPage from "./pages/blog.page";
 
 export const UserContext = createContext({})
 
@@ -33,6 +34,7 @@ const App = () => {
                 <Route path="/signup" element={<UserAuthForm type="sign-up"/>} />
                 <Route path="search/:query" element={<SearchPage />} />
                 <Route path="user/:id" element={<ProfilePage />} />
+                <Route path="blog/:blog_id" element={ <BlogPage /> } />
                 <Route path="*" element={<PageNotFound />} />
             </Route>
             
---
+++ frontend/src/pages/blog.page.jsx
@@ -0,0 +1,7 @@
+const BlogPage = () => {
+    return (
+        <h1>This is a blog page component jsk file</h1>
+    )
+}
+
+export default BlogPage;
